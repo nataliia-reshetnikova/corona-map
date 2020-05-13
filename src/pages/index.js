13cb8c9ceec2107c450f06df38aa8e3e6c5f308a
nataliereshetnikova <ab1748@gmail.com>
2020-05-13 14:16:51 -0400

    get data from https://corona.lmao.ninja/v2/countries with axios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
-
+import axios from 'axios';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
@@ -24,7 +24,14 @@ const IndexPage = () => {
 
   async function mapEffect({ leafletElement:map } = {}) {
     if(!map) return;
-  
+    let response;
+    try{
+      response = await axios.get('https://corona.lmao.ninja/v2/countries');
+    }catch (e){
+      console.log('E',e);
+      return;
+    }
+    console.log('response',response);
   }
 
   const mapSettings = {
