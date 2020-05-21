abd943401ccba0eed68c271838e99a1b44fb4722
HAMIDNGU <59899793+HAMIDNGU@users.noreply.github.com>
2020-05-21 16:47:21 -0400

    Update map-services.js
    added design foand attribute for page-2.js

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -8,5 +8,13 @@ export const mapServices = [
     name:'Mapbox',
     attribution:"© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     url:'https://api.mapbox.com/styles/v1/ab1748/cka75rpms0hvy1iqmu739u3cl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYWIxNzQ4IiwiYSI6ImNrYTJpb3A4cDAwNDIza28ya2x3cTdxd3MifQ.RNmsO3JHdFtYi5SvwEIp8A'
+  },
+
+ 
+  {
+    name: 'Mapbox2',
+    attribution: 'Map data &copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors, <a href=&quot;https://creativecommons.org/licenses/by-sa/2.0/&quot;>CC-BY-SA</a>, Imagery &copy; <a href=&quot;https://www.mapbox.com/&quot;>Mapbox</a>',
+    url: `https://api.mapbox.com/styles/v1/hamidrezash/ckafkfh7p0qtp1iqbj9yr6931/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGFtaWRyZXphc2giLCJhIjoiY2thZm4yNGwyMDBiaTJ1cG1heXg2NmFqbSJ9.JAJR-MiIXXA4j8igtmAHVw`
   }
 ];
+
